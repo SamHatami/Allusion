5617ec8a5bb2d3dd0874605b449d3614af30d280
Sammostache <mhatami@hotmail.com>
2024-09-24 18:24:53 +0200

    Custom Title Bars

--- Allusion/Views/MainView.xaml.cs
+++ Allusion/Views/MainView.xaml.cs
@@ -3,17 +3,59 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using Allusion.ViewModels;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
-namespace Allusion.Views
+namespace Allusion.Views;
+
+/// <summary>
+/// Interaction logic for MainView.xaml
+/// </summary>
+public partial class MainView : Window
 {
-    /// <summary>
-    /// Interaction logic for MainView.xaml
-    /// </summary>
-    public partial class MainView : Window
+    public MainView()
     {
-        public MainView()
+        InitializeComponent();
+    }
+
+    private void OnWindowInitialized(object sender, EventArgs e)
+    {
+        RefreshMaximizeRestoreButton();
+    }
+
+    private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.MainWindow.WindowState = WindowState.Minimized;
+    }
+
+    private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+    {
+        if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+        else if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+    }
+
+    private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void RefreshMaximizeRestoreButton()
+    {
+        if (WindowState == WindowState.Maximized)
         {
-            InitializeComponent();
+            MaximizeButton.Visibility = Visibility.Collapsed;
+            RestoreButton.Visibility = Visibility.Visible;
         }
+        else
+        {
+            MaximizeButton.Visibility = Visibility.Visible;
+            RestoreButton.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    private void WindowStateChangedHandler(object sender, EventArgs e)
+    {
+        RefreshMaximizeRestoreButton();
     }
 }
